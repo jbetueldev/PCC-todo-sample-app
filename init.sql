86f830eddd9bc7c23d39461c91e3f4363b9bd7f8
jbetueldev <johnbetuel.dev@gmail.com>
2024-08-20 16:23:41 +0800

    NodeJs + MySQL + Docker

--- init.sql
+++ init.sql
@@ -1,1 +1,7 @@
-ALTER USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY 'test'
+CREATE DATABASE test_db;
+
+CREATE USER 'test_user'@'%' IDENTIFIED WITH mysql_native_password BY 'test_password';
+
+GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'%';
+
+FLUSH PRIVILEGES;
